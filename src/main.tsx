e5edbc3f96a3502d83863ebfab3e274638305122
ale1983ale <alessio.cedroni@gmail.com>
2026-09-16 17:59:24 +0200

    Aggiungi sezione Invio Email Outlook
    * Add Microsoft Outlook draft dependencies
    
    * Add Outlook email draft workspace
    
    * Expose Outlook email workspace in app
    
    * Limit Outlook workspace to admin sessions

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import OutlookEmail from "./OutlookEmail";
+
+const hasAdminSession = Boolean(localStorage.getItem("admin_session"));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    {hasAdminSession && <OutlookEmail />}
   </React.StrictMode>
 );
